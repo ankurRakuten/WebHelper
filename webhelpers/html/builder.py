92fcb810744ca3c6437e8aaabb76c957e9ac4d95
ankurRakuten <48309275+ankurRakuten@users.noreply.github.com>
2019-07-31 16:30:34 +0530

    Update builder.py

--- webhelpers/html/builder.py
+++ webhelpers/html/builder.py
@@ -157,7 +157,8 @@ abuse ``_closed=False`` to produce them.
 """
 import re
 from urllib.parse import quote as url_escape
-from UserDict import DictMixin
+# from UserDict import DictMixin
+from collections import MutableMapping as DictMixin
 
 import markupsafe
 try:
